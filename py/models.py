Fix transformer masking

py/models.py
@@ -94,12 +94,16 @@ class TransformerEncoderContextualizer(Contextualizer):
         )
         self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=n_layers)
 
-    def forward(self, word_embeddings):
+    def forward(self, word_embeddings, mask=None):
         """
         Input: (B, 16, 384)
         Returns (B, 16, 384)
         """
-        return self.encoder(word_embeddings)
+        padding_mask = None
+        if mask is not None:
+            # Invert as our gamestate is true=valid, but in pytorch its true=ignore
+            padding_mask = ~mask 
+        return self.encoder(word_embeddings, src_key_padding_mask=padding_mask)
 
 class SimpleGrouper(Grouper):
 
@@ -272,9 +276,9 @@ class ConnectionsDQN(Model):
         word_embeddings = state['board']
         lives = state['lives']
         num_groups_found = state['num_groups_found']
-
+        word_mask = state.get('words_mask')
         # Contextualize
-        context_embeddings = self.contextualizer(word_embeddings)
+        context_embeddings = self.contextualizer(word_embeddings, mask=word_mask)
         
         # Group
         grouped_embeddings = self.grouper(16, context_embeddings) # (B, 1820, 4, D)
@@ -317,6 +321,9 @@ class ConnectionsDQN(Model):
         # Optimize
         self.optimizer.zero_grad()
         loss.backward()
+        # Gradient clipping
+        torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm=1.0)
+        # Update step
         self.optimizer.step()
         
         return loss.item()

py/replay.py
@@ -38,15 +38,19 @@ class ReplayMemory:
             self.next_lives = torch.zeros((self.cap, *next_state['lives'].shape), device=self.device, dtype=next_state['lives'].dtype)
             self.next_num_groups_found = torch.zeros((self.cap, *next_state['num_groups_found'].shape), device=self.device, dtype=next_state['num_groups_found'].dtype)
             self.finished = torch.zeros((self.cap, *finished.shape), device=self.device, dtype=finished.dtype)
+            self.m = torch.zeros((self.cap, *state['words_mask'].shape), device=self.device, dtype=torch.bool)
+            self.next_m = torch.zeros((self.cap, *next_state['words_mask'].shape), device=self.device, dtype=torch.bool)
 
         self.s[self.idx] = state['board']
         self.lives[self.idx] = state['lives']
         self.num_groups_found[self.idx] = state['num_groups_found']
+        self.m[self.idx] = state['words_mask']
         self.a[self.idx] = action
         self.r[self.idx] = reward
         self.sp[self.idx] = next_state['board']
         self.next_lives[self.idx] = next_state['lives']
         self.next_num_groups_found[self.idx] = next_state['num_groups_found']
+        self.next_m[self.idx] = next_state['words_mask']
         self.finished[self.idx] = finished
 
         self.idx = (self.idx + 1) % self.cap
@@ -64,13 +68,15 @@ class ReplayMemory:
         state = {
             'board': self.s[sample_idx],
             'lives': self.lives[sample_idx],
-            'num_groups_found': self.num_groups_found[sample_idx]
+            'num_groups_found': self.num_groups_found[sample_idx],
+            'words_mask': self.m[sample_idx]
         }
         
         next_state = {
             'board': self.sp[sample_idx],
             'lives': self.next_lives[sample_idx],
-            'num_groups_found': self.next_num_groups_found[sample_idx]
+            'num_groups_found': self.next_num_groups_found[sample_idx],
+            'words_mask': self.next_m[sample_idx]
         }
 
         return (