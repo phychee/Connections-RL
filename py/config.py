4094cacd743c48f6e24471e7261f0b853196a904
Zhen Sun <zhensun.us@gmail.com>
2025-12-05 01:08:08 -0800

    Attempt #9 with adjusted config

--- py/config.py
+++ py/config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class GameConfig:
     R1_win: float = 10.0
-    R2_correct: float = 1.0
+    R2_correct: float = 4.0
     R3_one_away: float = 0.2
     R4_wrong: float = -1
     R5_game_over: float = -10.0
